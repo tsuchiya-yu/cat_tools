12f174baf53f022ddc199c32b939fa520b2a343f
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2025-11-07 21:03:05 +0900

    lastModifiedをdate-fnsでフォーマット

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,30 +1,32 @@
 import { MetadataRoute } from 'next';
+import { formatISO } from 'date-fns';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://cat-tools.catnote.tokyo';
+  const lastModified = formatISO(new Date());
   
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: `${baseUrl}/calculate-cat-age`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/calculate-cat-calorie`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/calculate-cat-feeding`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
