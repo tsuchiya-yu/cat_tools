1049a285d2461816310db43a9bf211380bf2712e
tsuchiya-yu <37299387+tsuchiya-yu@users.noreply.github.com>
2025-11-08 23:23:13 +0900

    サイトマップにcalculate-cat-feedingを追加 (#35)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,23 +2,30 @@ import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://cat-tools.catnote.tokyo';
+  const lastModified = '2025-11-08';
   
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: `${baseUrl}/calculate-cat-age`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/calculate-cat-calorie`,
-      lastModified: new Date(),
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/calculate-cat-feeding`,
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
