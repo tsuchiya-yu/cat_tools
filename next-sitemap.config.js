283c1eaa72b01eb512afe1213361a4ba6668b578
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2025-12-22 15:37:47 +0900

    サイトマップ用URL未設定時にエラー化

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,12 @@
  */
 const tools = require('./src/constants/tools.json');
 
-const baseUrl = process.env.SITE_URL || process.env.NEXT_PUBLIC_BASE_URL || 'https://cat-tools.catnote.tokyo';
+const baseUrl = process.env.SITE_URL || process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!baseUrl) {
+  throw new Error('SITE_URL is not set. Please configure SITE_URL or NEXT_PUBLIC_BASE_URL.');
+}
+
 const siteUrl = baseUrl.startsWith('http') ? baseUrl : `https://${baseUrl.replace(/^\/\//, '')}`;
 const toolPaths = new Set(tools.map((tool) => tool.href));
 
