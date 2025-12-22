133a7c06df6735cc6283f7ca35f9cadd64a0f206
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2025-12-22 14:26:55 +0900

    chore: rely on auto lastmod in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,23 +7,4 @@ module.exports = {
   changefreq: 'monthly',
   priority: 0.7,
   autoLastmod: true,
-  transform: async (config, path) => {
-    const lastmod = new Date().toISOString();
-    const overrides = {
-      '/': { priority: 1 },
-      '/calculate-cat-age': { priority: 0.8 },
-      '/calculate-cat-calorie': { priority: 0.8 },
-      '/calculate-cat-feeding': { priority: 0.8 },
-      '/cat-food-safety': { priority: 0.8 },
-    };
-
-    const override = overrides[path];
-
-    return {
-      loc: path,
-      lastmod,
-      changefreq: 'monthly',
-      priority: override?.priority ?? config.priority,
-    };
-  },
 };
