8e5f2a3b35e9277c75ef57406514922becad9b59
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2026-02-24 09:32:47 +0900

    Normalize localhost BASE_URL for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL ?? 'http://127.0.0.1:3000',
+    baseURL:
+      process.env.BASE_URL?.replace('localhost', '127.0.0.1') ??
+      'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
