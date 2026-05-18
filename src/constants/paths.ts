fa991a60da6e03547a3b89b13aa48cb2df46cd72
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2026-05-18 18:55:59 +0900

    Add about page

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,5 +1,10 @@
+export const ABOUT_PATH = '/about';
 export const CAT_FOOD_SAFETY_PATH = '/cat-food-safety';
 export const CALCULATE_CAT_AGE_PATH = '/calculate-cat-age';
 export const CALCULATE_CAT_CALORIE_PATH = '/calculate-cat-calorie';
 export const CALCULATE_CAT_FEEDING_PATH = '/calculate-cat-feeding';
 export const CALCULATE_CAT_WATER_INTAKE_PATH = '/calculate-cat-water-intake';
+
+export const CONTACT_FORM_URL =
+  'https://docs.google.com/forms/d/e/1FAIpQLSeGH6AykLYDuxFasGFDUGGuCv72Ejfm1P1SDfPh2Q2RZVOzkg/viewform';
+export const X_PROFILE_URL = 'https://x.com/CATLINK_PR';
